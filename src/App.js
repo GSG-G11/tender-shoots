459da8aa230bb4dd9c5c7509806e6b06583e7964
Saleh T. Marouf <54906217+devstm@users.noreply.github.com>
2022-04-13 22:03:21 +0300

    Revert "create favorits route"

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-import Favorites from './Pages/favorites /Favorites';
-import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<h1>Home</h1>} />
         <Route path="/paint" element={<h1>Paint</h1>} />
-        <Route path="/favourites" element={<Favorites />} />
+        <Route path="/favourites" element={<h1>Favourites</h1>} />
       </Routes>
     </Router>
   );
